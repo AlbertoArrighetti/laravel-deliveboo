ba4d35b9d7e8a7227f4d78b16a64dc6d11f2feea
federico_viganò <fede_viga@hotmail.it>
2024-05-15 10:27:56 +0200

    feat theme dark

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container py-5">
         <h1>
             Benvenuto, {{$user->name}} {{$user->lastname}}
         </h1>
 
         <h2>
-            {{$restaurant->restaurant_name}}
+            il tuo ristorante:
         </h2>
 
-        <h2>
-            {{$restaurant->address}}
-        </h2>
+        <div class="px-3">
 
-        <h2>
-            {{$restaurant->image}}
-        </h2>
+            <h4>
+                {{$restaurant->restaurant_name}}
+            </h4>
+    
+            <h4>
+                {{$restaurant->address}}
+            </h4>
+    
+            <h4>
+                {{$restaurant->image}}
+            </h4>
+        </div>
     </div>
 @endsection
