15fb0c9d974f96147ad9478a8cf4ff303ed2f288
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-05-15 17:30:57 +0200

    fix:input price

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -27,7 +27,7 @@
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Prezzo</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') ?? $dish->price }}" required>
+            <input type="number" min="0" max="1000" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') ?? $dish->price }}" required>
             @error('price')
             <div class="invalid-feedback">
                 {{ $message }}
