40097c5fe131318b5592099de6aafe51d0b7e146
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-05-16 12:19:22 +0200

    feat: create & edit form required

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -8,8 +8,7 @@
             @csrf
 
             <div class="mb-3">
-                <h3>Nome del piatto</h3>
-                <label for="name" class="form-label">Nome</label>
+                <label for="name" class="form-label">Nome *</label>
                 <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{old('name')}}">
                 @error('name')
                 <div class="invalid-feedback">
@@ -19,13 +18,11 @@
             </div>
 
             <div class="mb-3">
-                <h3>Immagine del piatto</h3>
                 <label for="image">Inserisci un'immagine di copertina</label>
                 <input type="file" class="form-control" name="image">
             </div>
 
             <div class="mb-3">
-                <h3>Descrizione del piatto</h3>
                 <label for="description" class="form-label">Descrizione</label>
                 <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{old('description')}}</textarea>
                 @error('description')
@@ -36,8 +33,7 @@
             </div>
 
             <div class="mb-3">
-                <h3>Imposta il prezzo</h3>
-                <label for="price" class="form-label">Prezzo</label>
+                <label for="price" class="form-label">Prezzo *</label>
                 <input type="number" min="0" max="1000" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{old('price')}}">
                 @error('price')
                 <div class="invalid-feedback">
--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -8,7 +8,7 @@
         @csrf
         @method('PUT')
         <div class="mb-3">
-            <label for="name" class="form-label">Nome</label>
+            <label for="name" class="form-label">Nome *</label>
             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') ?? $dish->name }}" required>
             @error('name')
             <div class="invalid-feedback">
@@ -26,19 +26,18 @@
             @enderror
         </div>
         <div class="mb-3">
-            <label for="price" class="form-label">Prezzo</label>
+            <label for="image">Inserisci un'immagine di copertina</label>
+            <input type="file" class="form-control" name="image">
+            <img class="img-fluid py-2" src="{{asset('storage/' . $dish->image)}}">
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Prezzo *</label>
             <input type="number" min="0" max="1000" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') ?? $dish->price }}" required>
             @error('price')
             <div class="invalid-feedback">
                 {{ $message }}
             </div>
             @enderror
-        </div>
-        <div class="mb-3">
-            <h3>Immagine del piatto</h3>
-            <label for="image">Inserisci un'immagine di copertina</label>
-            <input type="file" class="form-control" name="image">
-            <img class="img-fluid py-2" src="{{asset('storage/' . $dish->image)}}">
         </div>
         <div class="mb-3 form-check">
             <input class="form-check-input" type="checkbox" id="viewable" name="viewable" value="1" {{ $dish->viewable ? 'checked' : '' }}>
