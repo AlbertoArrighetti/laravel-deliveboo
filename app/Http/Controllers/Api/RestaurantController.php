2cb22183878d5eefee4a02a653d74d468e251888
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-05-21 10:19:13 +0200

    fix: pagination

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -8,19 +8,9 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $typeFilter = $request->query('type');
-
-        $query = Restaurant::with(['types', 'dishes']);
-
-        if ($typeFilter) {
-            $query->whereHas('types', function ($query) use ($typeFilter) {
-                $query->where('name', $typeFilter);
-            });
-        }
-
-        $restaurants = $query->paginate(12);
+        $restaurants = Restaurant::with(['types', 'dishes'])->get();
 
         return response()->json([
             "success" => true,
