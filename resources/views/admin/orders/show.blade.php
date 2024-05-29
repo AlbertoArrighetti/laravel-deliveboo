13c6ffb03a68d83eb2c2942dfc88ab93a1e5ec05
mariasolegallina <gallinamariasole@gmail.com>
2024-05-29 16:40:55 +0200

    feat: order page layout

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -7,22 +7,37 @@
         <div class="page-title">
             {{-- go back to the dish list --}}
             <a class="btn btn1" href="{{route('admin.orders.index')}}"><i class="fa-solid fa-angle-left"></i> Ordini</a>
-            {{-- <h2 class="title">Ordine</h2> --}}
+            <h2 class="title">Dettagli ordine</h2>
         </div>
 
         <div class="d-flex align-items-start gap-3">
-            <div class="box-list flex-fill">
-                <p>Ordine effettuato il {{$order->created_at->format('d/m/Y')}} alle ore {{$order->created_at->format('H:i')}}</p>
-            
-            
-            
-            
-            
-            
+
+            {{-- ORDER --}}
+            <div class="flex-fill d-flex flex-column gap-3">
+                <div class="box-list">
+                    <p class="fw-semibold m-0">Ordine effettuato il {{$order->created_at->format('d/m/Y')}} alle ore {{$order->created_at->format('H:i')}}</p>                
+                </div>
+                <div class="box-list m-0">
+                    <ul class="d-flex flex-column gap-2 m-0 ps-0">
+                        @foreach ($dishes as $dish)
+                        <li class="d-flex align-items-center gap-2">
+                            <div class="circle">
+                                {{ $dish->pivot->where('dish_id', $dish->id)->where('order_id', $order->id)->first()->quantity}}
+                            </div>
+                            {{$dish->name}}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+    
+                <div class="box-list">
+                    <span class="fw-semibold">Totale:</span> € {{$order->total_price}}
+                </div>
             </div>
 
+            {{-- CUSTOMER --}}
             <div class="box-list col-4">
-                <ul class="d-flex flex-column gap-2 ps-0">
+                <ul class="d-flex flex-column gap-2 ps-0 m-0">
                     <li>
                         <span class="fw-semibold">Cliente</span>
                         <br>
@@ -45,29 +60,7 @@
                     </li>
                 </ul>
             </div>
-        </div>
-
-
-        <h2>
-            Dettagli ordine:
-        </h2>
-        <ul>
-            @foreach ($dishes as $dish)
-            <li>
-                {{$dish->name}}
-                x
-                {{ $dish->pivot->where('dish_id', $dish->id)->where('order_id', $order->id)->first()->quantity}}
-            </li>
-            @endforeach
-        </ul>
-
-        <div>Totale:</div>
-        <div class="mb-5">
-            € {{$order->total_price}}
-        </div>
-
-
-        <a href="{{route('admin.orders.index')}}" class="btn btn1">Ordini</a>
+        </div>    
 
     </div>
 </section>
