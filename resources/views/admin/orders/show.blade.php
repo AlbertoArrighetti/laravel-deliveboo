74b5d5fe8737fe8b32b8aca4f02a3b94f24e8fe3
mariasolegallina <gallinamariasole@gmail.com>
2024-05-29 16:09:29 +0200

    feat: order structure

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -4,26 +4,49 @@
 <section>
     <div class="container">
 
-        <div class="text-center">
-
-            <span class="text-center">
-                {{$order->created_at->format('d/m/Y')}}
-            </span>
-            <span class="text-center">
-                {{$order->created_at->format('H:i')}}
-            </span>
-        
+        <div class="page-title">
+            {{-- go back to the dish list --}}
+            <a class="btn btn1" href="{{route('admin.orders.index')}}"><i class="fa-solid fa-angle-left"></i> Ordini</a>
+            {{-- <h2 class="title">Ordine</h2> --}}
         </div>
 
-        <h2>
-            Dati cliente:
-        </h2>
-        <ul>
-            <li>{{$order->customer_name}} {{$order->customer_lastname}} </li>
-            <li>{{$order->customer_email}} </li>
-            <li>+39 {{$order->customer_phone}} </li>
-            <li>{{$order->customer_address}} </li>
-        </ul>
+        <div class="d-flex align-items-start gap-3">
+            <div class="box-list flex-fill">
+                <p>Ordine effettuato il {{$order->created_at->format('d/m/Y')}} alle ore {{$order->created_at->format('H:i')}}</p>
+            
+            
+            
+            
+            
+            
+            </div>
+
+            <div class="box-list col-4">
+                <ul class="d-flex flex-column gap-2 ps-0">
+                    <li>
+                        <span class="fw-semibold">Cliente</span>
+                        <br>
+                        {{$order->customer_name}} {{$order->customer_lastname}}
+                    </li>
+                    <li>
+                        <span class="fw-semibold">Indirizzo</span>
+                        <br>
+                        {{$order->customer_address}}
+                    </li>
+                    <li>
+                        <span class="fw-semibold">Email</span>
+                        <br>
+                        {{$order->customer_email}}
+                    </li>
+                    <li>
+                        <span class="fw-semibold">Telefono</span>
+                        <br>
+                        +39 {{$order->customer_phone}}
+                    </li>
+                </ul>
+            </div>
+        </div>
+
 
         <h2>
             Dettagli ordine:
