47efd986110abc28cc44cb200bd40cd6bfd05771
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-05-30 10:43:01 +0200

    fix: logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,29 +94,11 @@
                             document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                             </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                         </li>
 
-                        {{-- DROPDOWN MENU --}}
-                        {{-- <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-                        
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a>
-                                <a class="dropdown-item" href="{{route('admin.dishes.index')}}" class="btn btn1">Menù</a>
-                                <a class="dropdown-item" href="{{route('admin.orders.index')}}" class="btn btn1">Ordini</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                                             document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                        
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>                         --}}
                         @endguest
                     </ul>
                 </div>
