5cf7b12592615389f39332b2a4f03ed1d499de09
federico_viganò <fede_viga@hotmail.it>
2024-05-30 18:29:17 +0200

    wip mails

--- resources/views/emails/new-order.blade.php
+++ resources/views/emails/new-order.blade.php
@@ -1,49 +1,87 @@
 
-        <h1>
-            Grazie del tuo ordine, {{$order->customer_name}}!
-        </h1>
+<h1 class="text-center">
+    Grazie del tuo ordine, {{$order->customer_name}}!
+</h1>
 
-        <p>
-            Il tuo ordine verrà spedito a: {{$order->customer_address}} <br>
 
-            Dati Ristorante:
-            <ul>
-                <li>
-                   Nome: {{$restaurant->restaurant_name}} 
-                </li>
+<h2 class="text-center">
+    "{{$restaurant->restaurant_name}}" ha accettato il tuo ordine
+</h2>
 
-                <li>
-                  Indirizzo: {{$restaurant->address}} 
+<div class="text-center">
+    Il tuo ordine verrà spedito a: {{$order->customer_address}}
+</div>
 
-                </li>
-                <li>
-                   N° di telefono: {{$user->phone_number}}   
+<div class="container">
 
-                </li>
-                <li>
-                    P.IVA: {{$user->vat}}   
-
-                </li>
-                <li>
-                    E-mail: {{$user->email}}   
-
-                </li>
-            </ul>
-
-        </p>
-
-        <h1>Riepilogo Ordine</h1>
+    <div>
+        <h4>Riepilogo ordine:</h4>
 
         <ul>
-
             @foreach($order->dishes as $dish)
             <li>
                 {{ $dish->name }} - €{{ $dish->price }} x {{ $dish->pivot->where('dish_id', $dish->id)->where('order_id', $order->id)->first()->quantity}} = €{{ $dish->price *  $dish->pivot->where('dish_id', $dish->id)->where('order_id', $order->id)->first()->quantity}}
             </li>
             @endforeach
         </ul>
+    </div>
 
-        <p>
+    <div>
+        <h4>Dati Ristorante:</h4>
 
-            Totale ordine: €{{$order->total_price}} <br>
-        </p>
+        <ul>
+            <li>
+                Nome: {{$restaurant->restaurant_name}} 
+            </li>
+    
+            <li>
+                Indirizzo: {{$restaurant->address}} 
+    
+            </li>
+            <li>
+                N° di telefono: {{$user->phone_number}}   
+    
+            </li>
+            <li>
+                P.IVA: {{$user->vat}}   
+    
+            </li>
+            <li>
+                E-mail: {{$user->email}}   
+    
+            </li>
+        </ul>
+    </div>
+    <p>
+        <h4>Totale ordine:</h4>
+        <ul>
+            <li>
+                € {{$order->total_price}}
+            </li>
+        </ul> 
+    </p>
+</div>
+
+
+
+<style scoped>
+    * {
+        font-family: sans-serif;
+    }
+
+    .container {
+        padding: 10px;
+    }
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        margin-bottom: 1em;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+    
+</style>
